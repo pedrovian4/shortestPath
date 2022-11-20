b9e6b7f5ff15240f1103ca73d6b69edb1553d608
Pedro <pedrovianaasking@gmail.com>
2022-11-20 11:24:14 -0300

    Adicionado lógica de  de gração de saídas para não gerar em cima dos muros

--- js/obj/exit.js
+++ js/obj/exit.js
@@ -11,6 +11,15 @@ const exit ={
 
             this.xExit = Math.floor(Math.random()* 20);
             this.yExit = Math.floor(Math.random()* 20);
+            walls.positions.forEach(e=>{
+           
+                while (e.x == this.xExit && e.y==this.yExit){
+                    
+
+                    this.xExit = Math.floor(Math.random()* 20);
+                    this.yExit = Math.floor(Math.random()* 20);
+                }
+            });
         }
     }
 }
